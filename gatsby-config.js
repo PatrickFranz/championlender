911fff45d1acb6cc3ac7f026a88dbb6f792b6c1b
Patrick Franz <clymbing@gmail.com>
2021-04-26 12:18:29 -0700

    add: base setup complete style: added pri and sec colors to GlobalStyles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Champion Lender website`,
+    description: `Champion Lender was created to help people find the best loan for them while saving them time and money.`,
+    author: `@@patrickfranz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,7 +28,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/styles/Typography`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+:root{
+  //COLORS
+
+}
+
+body{
+  box-sizing: border-box;
+}
+  a{
+    text-decoration: none;
+    color: initial;
+  }
+`
