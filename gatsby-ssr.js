0b085e67703f615466581218cd3ead45f8c71317
Patrick Franz <clymbing@gmail.com>
2021-07-07 15:51:51 -0700

    fix: clean up gatsbyssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,22 +10,3 @@ export const wrapPageElement = ({ element, props }) => {
     </Layout>
   )
 }
-
-function setLeadId() {
-  var s = document.createElement('script')
-  s.id = 'LeadiDscript_campaign'
-  s.type = 'text/javascript'
-  s.async = true
-  s.src =
-    '//create.lidstatic.com/campaign/cd215e19-865f-3649-c858-0b6646c7cfa9.js?snippet_version=2'
-  var LeadiDscript = document.getElementById('LeadiDscript')
-  LeadiDscript.parentNode.insertBefore(s, LeadiDscript)
-}
-
-export const onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
-    <script id="LeadiDscript" type="text/javascript">
-      setLeadId()
-    </script>,
-  ])
-}
